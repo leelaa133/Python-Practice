ae240cb8971ba342a6119286c84315bf4655c12e
leelaa133 <sn1890048@gmail.com>
2026-06-05 11:31:35 +0530

    Added variables and input programs

--- Day-1 Basics/helloworld.py
+++ Day-1 Basics/helloworld.py
@@ -1,1 +1,12 @@
+#print using direct string
 print("hello world")
+
+#print using the variable assigning
+str = "Hello world"
+print(str)
+
+#print using the function
+def prnt_helo():
+    print("hello world, i am suresh")
+
+prnt_helo()
