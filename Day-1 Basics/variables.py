54a773c09e386effd0246cfd52c589f0bb7ea0a4
leelaa133 <sn1890048@gmail.com>
2026-06-05 13:06:16 +0530

    updated programs

---
+++ Day-1 Basics/variables.py
@@ -0,0 +1,30 @@
+#program 1
+
+name = "suresh"
+age = 30
+city = "Guntur"
+print("My name is", name)
+print("i am ", age, "years old")
+print("i live in ", city)
+
+#program2
+a=10 
+b=20
+print(a+b)
+
+#program2 another version
+a= int(input("enter value of a"))
+b= int(input("enter value of b"))
+print(a+b)
+
+# program 3
+L = float(input("enter length of the rectangle"))
+B = float(input("Enter width of the rectangle"))
+area = L * B
+print(area)
+print("area of rectangle", area)
+
+#program4
+r = int(input("enter radisu of the circle"))
+area = 3.14*r*r
+print(area)
